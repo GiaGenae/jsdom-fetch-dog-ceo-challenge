5e4dcfeeeb082250a2d410a09a7cf68717fa057b
Gia Jennings <gjennings.co@gmail.com>
2021-08-11 20:55:31 -0400

    commit tree be83059c63afeb3f0f90cd7ff9165b1cdb3207d9

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadImages();
+    loadBreeds();
+})
+
+function loadImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+    return fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(results => {
+        results.message.forEach(image => addImage(image))
+    });
+}
+
+function loadBreeds() {
+    const container = document.getElementById("dog-image-container");
+    const newImage = document.createElement('img');
+    newImage.src = picUrl;
+    container.appendChild(newImage);
+}
+    
+function addBreeds(breeds) {
+    const ul = document.getElementById("dog-breeds");
+    breeds.forEach(breed => {
+        const li = document.createElement("li");
+        li.innerText = breed;
+        ul.appendChild(li);
+        li.addEventListener("click", function(event) {
+            event.target.style.color = "blue";
+        });
+    });
+}
